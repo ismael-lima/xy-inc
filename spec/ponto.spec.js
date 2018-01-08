85875276c72d75761565757297343e2138550fbe
ismael-lima <ismael.j.lima@gmail.com>
2018-01-08 14:37:22 -0200

    Update ponto.spec.js

--- spec/ponto.spec.js
+++ spec/ponto.spec.js
@@ -1,6 +1,7 @@
 var request = require("request");
-var server = require("../server");
-var base_url = "http://localhost:3001/ponto";
+var server = require("../testserver");
+var base_url = "http://localhost:3021/ponto";
+
 
 describe("XY Inc tests", () => {
     
@@ -90,70 +91,102 @@ describe("XY Inc tests", () => {
     });
     
     describe("Listar registros", () => {
-        it("Listar registros", (done) =>{
-            request.get(base_url, (error, response, body) => {
-                expect(response.statusCode).toBe(200);
-                let retorno = JSON.parse(body);
-                expect(retorno.length).toBe(7);
-                done();
+        it("Listar registros", (done) =>{            
+            request.get(base_url+'/clear', (error, response, body) => {
+                request.post(base_url,{form:{nome:'Lanchonete', x :27, y:12}}, (error, response, body) => {                        
+                    request.post(base_url,{form:{nome:'Posto', x :31, y:18}}, (error, response, body) => {                       
+                        request.post(base_url,{form:{nome:'Joalheria', x :-15, y:12}}, (error, response, body) => {                            
+                            request.post(base_url,{form:{nome:'Floricultura', x :19, y:21}}, (error, response, body) => {                            
+                                request.post(base_url,{form:{nome:'Pub', x :12, y:8}}, (error, response, body) => {                                
+                                    request.post(base_url,{form:{nome:'Supermercado', x :23, y:6}}, (error, response, body) => {                                    
+                                        request.post(base_url,{form:{nome:'Churrascaria', x :28, y:2}}, (error, response, body) => {                                        
+                                            request.post(base_url,{form:{nome:'Clube de Xadrez', x :28, y:-2}}, (error, response, body) => {
+                                                request.get(base_url, (error, response, body) => {
+                                                    expect(response.statusCode).toBe(200);
+                                                    let retorno = JSON.parse(body);
+                                                    console.log(retorno);
+                                                    expect(retorno.length).toBe(6);
+                                                    done();
+                                                });                    
+                                            }); 
+                                        }); 
+                                    }); 
+                                }); 
+                            });
+                        }); 
+                    });
+                });                 
             });
         });
     });
     
     describe("Listar pontos proximos", () => {
-        it("Listar pontos proximos", (done) =>{
-            request.post(base_url+'/proximidade',{form:{distancia:10,x: 20,y: 10}}, (error, response, body) => {
-                expect(response.statusCode).toBe(200);
-                let retorno = JSON.parse(body);
-                expect(retorno.success).toBe(true);
-                expect(retorno.lista.length).toBe(4);
-                done();
+        it("Listar pontos proximos", (done) =>{            
+            request.get(base_url+'/clear', (error, response, body) => {
+                request.post(base_url,{form:{nome:'Lanchonete', x :27, y:12}}, (error, response, body) => {                        
+                    request.post(base_url,{form:{nome:'Posto', x :31, y:18}}, (error, response, body) => {                       
+                        request.post(base_url,{form:{nome:'Joalheria', x :15, y:12}}, (error, response, body) => {                            
+                            request.post(base_url,{form:{nome:'Floricultura', x :19, y:21}}, (error, response, body) => {                            
+                                request.post(base_url,{form:{nome:'Pub', x :12, y:8}}, (error, response, body) => {                                
+                                    request.post(base_url,{form:{nome:'Supermercado', x :23, y:6}}, (error, response, body) => {                                    
+                                        request.post(base_url,{form:{nome:'Churrascaria', x :28, y:2}}, (error, response, body) => {                                        
+                                            request.post(base_url,{form:{nome:'Clube de Xadrez', x :28, y:-2}}, (error, response, body) => {
+                                                request.post(base_url+'/pontosproximos',{form:{distancia:10,x: 20,y: 10}}, (error, response, body) => {
+                                                    expect(response.statusCode).toBe(200);
+                                                    let retorno = JSON.parse(body);
+                                                    expect(retorno.success).toBe(true);
+                                                    expect(retorno.lista.length).toBe(4);
+                                                    done();
+                                                });
+                                            }); 
+                                        }); 
+                                    }); 
+                                }); 
+                            });
+                        }); 
+                    });
+                });                 
             });
         });
     });
-    
-    describe("inserir pontos ramdomicos", () => {
-        for(let cont = 0; cont<1000; cont++)
-        {
-            let xt = Math.floor(Math.random() * 500);
-            let yt = Math.floor(Math.random() * 500);
+        
+    describe("Listar pontos proximos randomicos", () => {
+        
+        request.get(base_url+'/clear', (error, response, body) => {
+            for(let cont = 0; cont<1000; cont++)
+            {
+                let xt = Math.floor(Math.random() * 500);
+                let yt = Math.floor(Math.random() * 500);
 
-            let ponto = {nome:'Ponto Teste '+cont, 
-                        x :xt, 
-                        y:yt};
+                let ponto = {nome:'Ponto Teste '+cont, 
+                            x :xt, 
+                            y:yt};
 
-            it("inserir ponto X: "+xt+", Y: "+yt, (done) =>{
                 request.post(base_url,{form: ponto}, (error, response, body) => {
-                    expect(response.statusCode).toBe(200);
-                    let retorno = JSON.parse(body);
-                    expect(retorno.success).toBe(true);
-                    done();
                 });
-            });
-        }
-    });
-    
-    describe("Listar pontos proximos ramdomicos", () => {
-        for(let cont = 0; cont<100; cont++)
-        {
-            let xt = Math.floor(Math.random() * 500);
-            let yt = Math.floor(Math.random() * 500);
-            let dt = Math.floor(Math.random() * 100);
+            }
 
-            it("Listar pontos na distancia de "+dt+" do ponto X: "+xt+", Y: "+yt, (done) =>{
-                request.post(base_url+'/proximidade',{form: {distancia:dt,x: xt,y: yt}}, (error, response, body) => {
-                    expect(response.statusCode).toBe(200);
-                    let retorno = JSON.parse(body);
-                    expect(retorno.success).toBe(true);
-                    retorno.lista.forEach((item)=>{
-                        let a = Math.pow(Math.abs(xt - item.x),2) + Math.pow(Math.abs(yt - item.y),2);
-                        let h = Math.pow(dt, 2);
-                        expect(a).not.toBeGreaterThan(h);
+            for(let cont = 0; cont<100; cont++)
+            {
+                let xt = Math.floor(Math.random() * 500);
+                let yt = Math.floor(Math.random() * 500);
+                let dt = Math.floor(Math.random() * 100);
+
+                it("Listar pontos na distancia de "+dt+" do ponto X: "+xt+", Y: "+yt, (done) =>{
+                    request.post(base_url+'/pontosproximos',{form: {distancia:dt,x: xt,y: yt}}, (error, response, body) => {
+                        expect(response.statusCode).toBe(200);
+                        let retorno = JSON.parse(body);
+                        expect(retorno.success).toBe(true);
+                        retorno.lista.forEach((item)=>{
+                            let a = Math.pow(Math.abs(xt - item.x),2) + Math.pow(Math.abs(yt - item.y),2);
+                            let h = Math.pow(dt, 2);
+                            expect(a).not.toBeGreaterThan(h);
+                        });
+                        done();
                     });
-                    done();
                 });
-            });
-        }
+            }
+        });      
     });
 
 });
